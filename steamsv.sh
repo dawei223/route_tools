76be76f0df05c21f5f6e5c6521e8debbd90cea97
dawei223 <62640896+dawei223@users.noreply.github.com>
2021-08-17 12:46:07 +0800

    Update steamsv.sh

--- steamsv.sh
+++ steamsv.sh
@@ -28,5 +28,4 @@ fi
 exit 0
 
 计划任务中添加
-* * * * * /etc/config/steamsv.sh > /tmp/steamsv.log
-*/5 * * * * /etc/config/steamsv.sh > /tmp/steamsv.log
+*/10 * * * * /etc/config/steamsv.sh > /tmp/steamsv.log
